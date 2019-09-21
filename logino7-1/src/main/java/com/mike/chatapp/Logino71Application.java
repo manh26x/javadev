86357e3372e1e9c0179f2ff8b6e49b5667af3b50
manh26x <hungmanh2607@gmail.com>
2019-09-22 02:01:25 +0700

    Add files via upload

---
+++ logino7-1/src/main/java/com/mike/chatapp/Logino71Application.java
@@ -0,0 +1,13 @@
+package com.mike.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Logino71Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Logino71Application.class, args);
+	}
+
+}
